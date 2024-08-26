15d021c2ae923e04bb9998850f026122baf224cd
JrSchoolAccount <144328779+JrSchoolAccount@users.noreply.github.com>
2024-08-26 09:20:53 +0200

    Basic Menu + input class

---
+++ src/main/java/lab1/Main.java
@@ -0,0 +1,18 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Elpriser\n" +
+                "========\n" +
+                "1. Inmatning\n" +
+                "2. Min, Max och Medel\n" +
+                "3. Sortera\n" +
+                "4. Bästa Laddningstid (4h)\n" +
+                "e. Avsluta");
+    }
+}
