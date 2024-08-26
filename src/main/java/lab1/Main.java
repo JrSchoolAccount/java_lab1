8d03b4064cda4967172fafc1cd3f0d7b257dda7a
JrSchoolAccount <144328779+JrSchoolAccount@users.noreply.github.com>
2024-08-26 10:16:44 +0200

    Set and menu loop with exit

--- src/main/java/lab1/Main.java
+++ src/main/java/lab1/Main.java
@@ -1,18 +1,51 @@
 package lab1;
 
+import java.util.HashSet;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
     public static Scanner sc = new Scanner(System.in);
+    private static final Set<String> VALID_INPUTS = new HashSet<>();
+
+    static {
+        VALID_INPUTS.add("1");
+        VALID_INPUTS.add("2");
+        VALID_INPUTS.add("3");
+        VALID_INPUTS.add("4");
+        VALID_INPUTS.add("e");
+    }
 
     public static void main(String[] args) {
-        System.out.println("Elpriser\n" +
-                "========\n" +
-                "1. Inmatning\n" +
-                "2. Min, Max och Medel\n" +
-                "3. Sortera\n" +
-                "4. Bästa Laddningstid (4h)\n" +
-                "e. Avsluta");
+        menu();
+    }
+
+    public static void menu() {
+        while (true) {
+            System.out.println(
+                            "Elpriser\n" +
+                            "========\n" +
+                            "1. Inmatning\n" +
+                            "2. Min, Max och Medel\n" +
+                            "3. Sortera\n" +
+                            "4. Bästa Laddningstid (4h)\n" +
+                            "e. Avsluta");
+
+            String input = sc.nextLine().toLowerCase();
+
+            try {
+                if (!VALID_INPUTS.contains(input)) {
+                    throw new IllegalArgumentException("Invalid input: " + input);
+                }
+
+                if (input.equals("e")) {
+                    break;
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println("You can't do that. Try again.");
+            }
+        }
     }
 }
