c4200987eda6795b3a4744d561a4731c5b33ce78
JrSchoolAccount <144328779+JrSchoolAccount@users.noreply.github.com>
2024-08-28 09:16:37 +0200

    added final

--- src/main/java/lab1/Price.java
+++ src/main/java/lab1/Price.java
@@ -1,8 +1,8 @@
 package lab1;
 
 public class Price {
-    private String hour;
-    private int price;
+    private final String hour;
+    private final int price;
 
 
     Price(int price, String hour) {
